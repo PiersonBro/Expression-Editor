0de75c84b2cde90b3ef8c50423f89bb29d36516c
PiersonBro <manit8525@gmail.com>
2016-10-21 23:00:55 -0700

    Outline UI.

--- ExpressionEditor/ViewController.swift
+++ ExpressionEditor/ViewController.swift
@@ -9,17 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let textEditor = UITextView(frame: CGRect())
+    let resultsPane = UIView(frame: CGRect())
+    let statusBarView = UIView(frame: CGRect())
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        configureResultsPaneLayout()
+    }
+    
+    func configureTextEditorLayout() {
+        view.backgroundColor = .white
+        view.addSubview(textEditor)
+        textEditor.translatesAutoresizingMaskIntoConstraints = false
+        
+        textEditor.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        configureResultsPaneLayout()
+        textEditor.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        textEditor.rightAnchor.constraint(equalTo: resultsPane.leftAnchor).isActive = true
+        
+        resultsPane.layer.masksToBounds = true
+        resultsPane.layer.cornerRadius = 15.0
+    }
+    
+    override func viewDidLayoutSubviews() {
+        textEditor.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
+        resultsPane.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
+        configureStatusBarView()
+    }
+    
+    func configureStatusBarView() {
+        statusBarView.backgroundColor = .white
+        statusBarView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(statusBarView)
+        statusBarView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        statusBarView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        statusBarView.bottomAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
+        statusBarView.topAnchor.constraint(equalTo: topLayoutGuide.topAnchor).isActive = true
+    }
+    
+    func configureResultsPaneLayout() {
+        view.addSubview(resultsPane)
+        resultsPane.translatesAutoresizingMaskIntoConstraints = false
+        resultsPane.backgroundColor = .gray
+
+        resultsPane.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        NSLayoutConstraint(item: resultsPane, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 0.3, constant: 0).isActive = true
+        NSLayoutConstraint(item: resultsPane, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 2.0, constant: 0).isActive = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
